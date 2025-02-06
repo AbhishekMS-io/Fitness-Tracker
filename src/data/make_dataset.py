d08f035e7e6a051dcdc7ea1851ac367ab9ab757a
AbhishekMS-io <aa4709513@gmail.com>
2025-02-06 15:53:10 +0530

    dataset manipulation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -88,11 +88,70 @@ gyr_df
 # Working with datetimes
 # --------------------------------------------------------------
 
+acc_df.dtypes
+
+pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+
+acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+gyr_df["epoch (ms)"]
+
+gyr_df
+
+gyr_df.drop(columns=["time (01:00)", "epoch (ms)", "elapsed (s)"], inplace=True)
+
+acc_df.drop(columns=["time (01:00)", "epoch (ms)", "elapsed (s)"], inplace=True)
+
+acc_df
+
+gyr_df
 
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
 
+files = glob("../../data/raw/MetaMotion/*.csv")
+
+
+def read_data_from_files(files):
+    data_path = "../../data/raw/MetaMotion\\"
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+
+    acc_set = 1
+    gyr_set = 1
+
+    for f in files:
+        participant = f.split("-")[0].replace(data_path, "")
+        label = f.split("-")[1]
+        category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+
+        df = pd.read_csv(f)
+        df["participant"] = participant
+        df["category"] = category
+        df["label"] = label
+
+        if "Accelerometer" in f:
+            df["set"] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+
+        if "Gyroscope" in f:
+            df["set"] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+
+    acc_df.index = pd.to_datetime(acc_df["epoch (ms)"], unit="ms")
+    gyr_df.index = pd.to_datetime(gyr_df["epoch (ms)"], unit="ms")
+
+    gyr_df.drop(columns=["time (01:00)", "epoch (ms)", "elapsed (s)"], inplace=True)
+    acc_df.drop(columns=["time (01:00)", "epoch (ms)", "elapsed (s)"], inplace=True)
+
+    return acc_df, gyr_df
+
+
+acc_df, gyr_df = read_data_from_files(files=files)
 
 # --------------------------------------------------------------
 # Merging datasets
